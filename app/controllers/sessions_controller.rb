41fd60ac717127dd4456523b5233ffa8da243938
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2016-07-10 17:55:56 +0100

    Improve login process
    - If you try to sign up with a taken email, suggest logging in
    - Prevent login attempts with blank email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,17 @@ class SessionsController < ApplicationController
   before_action :require_no_login, except: :destroy
 
   def new
+    @email = params[:email]
   end
 
   def create
     @email = params[:email]
+
+    if @email.blank?
+      flash.now[:alert] = "Email address can't be blank"
+      render "new"
+      return
+    end
 
     user = User.where(email: params[:email]).first
 
