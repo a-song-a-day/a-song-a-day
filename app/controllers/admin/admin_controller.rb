b01444058b6199594c3726860e16d087bc7d52aa
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2016-07-23 00:44:09 +0100

    Crumbs!

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,4 +2,18 @@ class Admin::AdminController < ApplicationController
   before_action :require_login
 
   layout 'admin'
+  
+  private
+
+  def crumb(name, path)
+    crumbs.push(Crumb.new(name, path))
+  end
+
+  helper_method :crumb
+
+  def crumbs
+    @crumbs ||= []
+  end
+
+  helper_method :crumbs
 end
--- app/models/curator.rb
+++ app/models/curator.rb
@@ -1,6 +1,7 @@
 class Curator < ApplicationRecord
   belongs_to :user
   has_many :songs, -> { order('created_at DESC') }
+  has_many :subscriptions, -> { order('created_at DESC') }
 
   validates_presence_of :user, :title, :description
   validates_uniqueness_of :random, conditions: -> { where(random: true) }
