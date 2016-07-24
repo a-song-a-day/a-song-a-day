e32e4b15ce274d05e49dcc6824589741edbb99f2
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2016-07-24 20:32:17 +0100

    Add unit test for User#social_links

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,35 @@ class UserTest < ActiveSupport::TestCase
       second = User.create(name: 'Example', email: 'test@example.com')
     end
   end
+
+  test 'social links' do
+    user = User.new
+
+    assert_equal [], user.social_links
+
+    user.twitter_url = "https://twitter.com/alisdair"
+
+    assert_equal [["Twitter", "https://twitter.com/alisdair"]], user.social_links
+
+    user.instagram_url = ""
+
+    assert_equal [["Twitter", "https://twitter.com/alisdair"]], user.social_links
+
+    user.soundcloud_url = "whatever"
+
+    assert_equal [
+      ["Twitter", "https://twitter.com/alisdair"],
+      ["Soundcloud", "whatever"]
+    ], user.social_links
+
+    user.instagram_url = "photos"
+    user.spotify_url = "spotify"
+
+    assert_equal [
+      ["Twitter", "https://twitter.com/alisdair"],
+      ["Instagram", "photos"],
+      ["Spotify", "spotify"],
+      ["Soundcloud", "whatever"]
+    ], user.social_links
+  end
 end
