c9d0f684f2ef6336ea2c8c35be98c93e1af1e990
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2016-08-12 00:24:29 +0100

    Show PG-rated Gravatars

--- app/helpers/gravatar_url_helper.rb
+++ app/helpers/gravatar_url_helper.rb
@@ -3,6 +3,6 @@ require 'digest/md5'
 module GravatarUrlHelper
   def gravatar_url(email)
     hash = Digest::MD5.hexdigest(email.downcase)
-    "https://www.gravatar.com/avatar/#{hash}?s=400&d=blank"
+    "https://www.gravatar.com/avatar/#{hash}?s=400&d=blank&r=pg"
   end
 end
