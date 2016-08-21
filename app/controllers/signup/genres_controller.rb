8d4caf8bf85b96e77369bc6492fd71b3d6114467
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2016-08-21 12:21:11 +0100

    Don't allow multiple subscriptions

--- app/controllers/signup/genres_controller.rb
+++ app/controllers/signup/genres_controller.rb
@@ -2,6 +2,11 @@ class Signup::GenresController < ApplicationController
   before_action :require_login
 
   def new
+    if current_user.subscriptions.any?
+      redirect_to admin_subscriptions_path
+      return
+    end
+
     @genres = Genre.where(id: Curator.all.pluck(:genre_id).uniq).order(:name)
 
     render :form
