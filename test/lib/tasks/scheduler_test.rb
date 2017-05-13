131d726449728f81f82b14a5e103b03e82c5f566
Thomas R. Koll <tomk32@tomk32.de>
2017-05-13 15:26:23 +0200

    Maybe tests are not run in order, causing that scheduler test to fail?

--- test/lib/tasks/scheduler_test.rb
+++ test/lib/tasks/scheduler_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class SchedulerTaskTest < ActiveSupport::TestCase
   setup do
-    Timecop.travel(Time.new(2017, 5, 12, 10, 0, 0)) # Friday the 12th
     ASongADay::Application.load_tasks
   end
   test 'daily song' do
+    Timecop.travel(Time.new(2017, 5, 12, 10, 0, 0)) # Friday the 12th
     song = songs(:two_moons)
     subscriptions(:thomas_electropop)
     assert_nil song.reload.sent_at
     Rake::Task['daily_song'].invoke
     assert_not_nil song.reload.sent_at
+    Timecop.return
   end
 
   test 'daily song on a Saturday' do
