49d67edb4ded669003e759ffd38a870e5cb0a9ef
Thomas R. Koll <tomk32@tomk32.de>
2017-05-22 21:28:56 +0300

    Just cosmetic, change variable name in SendSongToSubscriptionWorker

--- app/workers/send_song_to_subscription_worker.rb
+++ app/workers/send_song_to_subscription_worker.rb
@@ -4,14 +4,14 @@ class SendSongToSubscriptionWorker
   # Exponential backoff: 11 retries is about 12 hours
   sidekiq_options retry: 11
 
-  def perform(song_id, subscription_id, date, day, daily_message = nil)
+  def perform(song_id, subscription_id, date, day, daily_message_id = nil)
     song = Song.find(song_id)
     subscription = Subscription.find(subscription_id)
 
     return if song.sent_subscription_ids.include? subscription.id
     return unless subscription.user.confirmed_email?
 
-    SubscriptionMailer.song(song, subscription, date, day, daily_message).deliver
+    SubscriptionMailer.song(song, subscription, date, day, daily_message_id).deliver
     song.append_sent_subscription_id subscription.id
 
   rescue ActiveRecord::RecordNotFound
